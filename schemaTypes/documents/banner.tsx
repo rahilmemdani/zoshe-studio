968456224ab143c0803e9ef94cfec8f16efeac17
rahilmemdani <rmemdanib@gmail.com>
2025-09-06 14:35:37 +0530

    Added banner

---
+++ schemaTypes/documents/banner.tsx
@@ -0,0 +1,51 @@
+// schemas/banner.ts
+import { defineField, defineType } from 'sanity'
+
+export const bannerType = defineType({
+  name: 'banner',
+  title: 'Banner',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'subtitle',
+      title: 'Subtitle',
+      type: 'string',
+    }),
+    defineField({
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+    }),
+    defineField({
+      name: 'ctaText',
+      title: 'CTA Text',
+      type: 'string',
+    }),
+    defineField({
+      name: 'ctaLink',
+      title: 'CTA Link',
+      type: 'url',
+    }),
+    defineField({
+      name: 'isActive',
+      title: 'Active',
+      type: 'boolean',
+      initialValue: true,
+    }),
+    defineField({
+      name: 'startDate',
+      title: 'Start Date',
+      type: 'datetime',
+    }),
+    defineField({
+      name: 'endDate',
+      title: 'End Date',
+      type: 'datetime',
+    }),
+  ],
+})
